419a12679ae7154bf9b5becf5fb996fca1ea8312
Radek Ermel <radek.ermel@assurity.co.nz>
2019-12-11 09:53:01 +1300

    initial setup

---
+++ src/com/tutorial/game/Game.java
@@ -0,0 +1,19 @@
+package com.tutorial.game;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public synchronized void start() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
